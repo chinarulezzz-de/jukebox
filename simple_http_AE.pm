34a31c5bfef4ffdbe77d396e01d43d18a2a68bba
Alexandr Savca <alexandr.savca89@gmail.com>
2020-08-20 20:43:46 +0300

    simple_http_AE.pm: restyle, gmusicbrowser -> jukebox

--- simple_http_AE.pm
+++ simple_http_AE.pm
@@ -1,13 +1,17 @@
-# Copyright (C) 2010-2011 Quentin Sculo <squentin@free.fr>
+# Copyright (c) Quentin Sculo  <squentin@free.fr>
+# Copyright (c) Alexandr Savca <drop@chinarulezzz.fun>
 #
-# This file is part of Gmusicbrowser.
-# Gmusicbrowser is free software; you can redistribute it and/or modify
+# This file is part of jukebox.
+#
+# jukebox is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3, as
 # published by the Free Software Foundation
 
 package Simple_http;
+
 use strict;
 use warnings;
+
 use AnyEvent::HTTP;
 my $UseCache = *GMB::Cache::add{CODE};
 
@@ -173,3 +177,6 @@ sub abort {
 }
 
 1;
+
+# vim:sw=4:ts=4:sts=4:et:cc=80
+# End of file

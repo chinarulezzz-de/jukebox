0faa0c1fbbf8b780a15592abe269779ccfe6754b
Alexandr Savca <alexandr.savca89@gmail.com>
2020-08-20 20:42:05 +0300

    oggheader.pm: restyle, gmusicbrowser -> jukebox

--- oggheader.pm
+++ oggheader.pm
@@ -1,7 +1,9 @@
-# Copyright (C) 2005-2009 Quentin Sculo <squentin@free.fr>
+# Copyright (c) Quentin Sculo  <squentin@free.fr>
+# Copyright (c) Alexandr Savca <drop@chinarulezzz.fun>
 #
-# This file is part of Gmusicbrowser.
-# Gmusicbrowser is free software; you can redistribute it and/or modify
+# This file is part of jukebox.
+#
+# jukebox is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3, as
 # published by the Free Software Foundation
 
@@ -12,6 +14,7 @@ package Tag::OGG;
 
 use strict;
 use warnings;
+
 use Encode qw(decode encode);
 use MIME::Base64;
 
@@ -618,3 +621,6 @@ sub _skip_to_last_page {
 }
 
 1;
+
+# vim:sw=4:ts=4:sts=4:et:cc=80
+# End of file

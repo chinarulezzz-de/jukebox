636f041debf8b76a8142a12325c836fc07ed5dc6
Alexandr Savca <alexandr.savca89@gmail.com>
2020-08-20 21:07:58 +0300

    lyrics.pm: restyle, gmusicbrowser -> jukebox

--- plugins/lyrics.pm
+++ plugins/lyrics.pm
@@ -1,7 +1,9 @@
-# Copyright (C) 2005-2009 Quentin Sculo <squentin@free.fr>
+# Copyright (c) Quentin Sculo  <squentin@free.fr>
+# Copyright (c) Alexandr Savca <drop@chinarulezzz.fun>
 #
-# This file is part of Gmusicbrowser.
-# Gmusicbrowser is free software; you can redistribute it and/or modify
+# This file is part of jukebox.
+#
+# jukebox is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3, as
 # published by the Free Software Foundation
 
@@ -992,7 +994,7 @@ sub Save_text {
     }
 }
 
-1
+1;
 
 # vim:sw=4:ts=4:sts=4:et:cc=80
 # End of file

6fc7d72101c25b02f4bbda720d16c9491b2df8c9
Alexandr Savca <alexandr.savca89@gmail.com>
2020-08-20 20:49:41 +0300

    wvheader.pm: restyle, gmusicbrowser -> jukebox

--- wvheader.pm
+++ wvheader.pm
@@ -1,7 +1,9 @@
-# Copyright (C) 2007 Quentin Sculo <squentin@free.fr>
+# Copyright (c) Quentin Sculo  <squentin@free.fr>
+# Copyright (c) Alexandr Savca <drop@chinarulezzz.fun>
 #
-# This file is part of Gmusicbrowser.
-# Gmusicbrowser is free software; you can redistribute it and/or modify
+# This file is part of jukebox.
+#
+# jukebox is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3, as
 # published by the Free Software Foundation
 
@@ -71,3 +73,5 @@ sub _ReadHeader {
 
 1;
 
+# vim:sw=4:ts=4:sts=4:et:cc=80
+# End of file

d40112918d0c3e63a3961f334764450aecf10195
Alexandr Savca <alexandr.savca89@gmail.com>
2020-08-20 20:59:46 +0300

    export.pm: restyle, gmusicbrowser -> jukebox

--- plugins/export.pm
+++ plugins/export.pm
@@ -1,7 +1,9 @@
-# Copyright (C) 2005-2008 Quentin Sculo <squentin@free.fr>
+# Copyright (c) Quentin Sculo  <squentin@free.fr>
+# Copyright (c) Alexandr Savca <drop@chinarulezzz.fun>
 #
-# This file is part of Gmusicbrowser.
-# Gmusicbrowser is free software; you can redistribute it and/or modify
+# This file is part of jukebox.
+#
+# jukebox is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3, as
 # published by the Free Software Foundation
 
@@ -279,4 +281,7 @@ sub RunCommand {
 }
 
 
-1
+1;
+
+# vim:sw=4:ts=4:sts=4:et:cc=80
+# End of file

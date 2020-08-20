96757e77af01604fdb9cfbae1a5e3a0d4e711bbd
Alexandr Savca <alexandr.savca89@gmail.com>
2020-08-20 21:17:06 +0300

    webcontext.pm: restyle, gmusicbrowser -> jukebox

--- plugins/webcontext.pm
+++ plugins/webcontext.pm
@@ -1,7 +1,9 @@
-# Copyright (C) 2005-2009 Quentin Sculo <squentin@free.fr>
+# Copyright (c) Quentin Sculo  <squentin@free.fr>
+# Copyright (c) Alexandr Savca <drop@chinarulezzz.fun>
 #
-# This file is part of Gmusicbrowser.
-# Gmusicbrowser is free software; you can redistribute it and/or modify
+# This file is part of jukebox.
+#
+# jukebox is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3, as
 # published by the Free Software Foundation
 
@@ -976,5 +978,7 @@ sub preset_menu_cb {
 }
 
 
-1
+1;
 
+# vim:sw=4:ts=4:sts=4:et:cc=80
+# End of file
